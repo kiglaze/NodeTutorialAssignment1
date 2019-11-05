eabbb6299b3877e893387c0d7aef06b8e3c76b98
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-11-04 20:09:17 -0500

    Added form and form submit route functionality.

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
@@ -6,6 +8,10 @@ const requestHandler = (req, res) => {
         res.write('<head><title>Assignment 1</title></head>');
         res.write('<body>');
         res.write('<p>Hi, how are you doing today?</p>');
+        res.write('<form action="/create-user" method="POST">');
+        res.write('<input type="text" name="username">');
+        res.write('<button type="submit">Submit</button>');
+        res.write('</form>');
         res.write('</body>');
         res.write('</html>');
         return res.end();
@@ -24,7 +30,19 @@ const requestHandler = (req, res) => {
         return res.end();
     }
     if (url === '/create-user' && method === 'POST') {
-
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const username = parsedBody.split('=')[1];
+            console.log(username);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
